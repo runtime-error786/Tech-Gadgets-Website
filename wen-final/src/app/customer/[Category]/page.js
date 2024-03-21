d56052b1d3e548c98b88f64886f503f637227301
runtime-error786 <mustafa782a@gmail.com>
2024-03-22 02:02:51 +0500

    add no product found message

--- wen-final/src/app/customer/[Category]/page.js
+++ wen-final/src/app/customer/[Category]/page.js
@@ -18,7 +18,7 @@ import "./Style.css";
 import Pagination from "../../admin/Others/Paging";
 import SortControls from "../Others/Sort";
 
-const Home = ({params}) => {
+const Home = ({ params }) => {
 
   const Prod = useSelector((state) => state.Record);
   const SearchProd = useSelector((state) => state.SearchUser);
@@ -28,12 +28,12 @@ const Home = ({params}) => {
   const dispatch = useDispatch();
   console.log(params.Category)
   useEffect(() => {
-    dispatch(ShowAllProdCus(SearchProd, SortProd, currentPage,params.Category));
-  }, [SearchProd, SortProd, currentPage,params.Category]);
+    dispatch(ShowAllProdCus(SearchProd, SortProd, currentPage, params.Category));
+  }, [SearchProd, SortProd, currentPage, params.Category]);
 
   useEffect(() => {
     dispatch(NextPage(0));
-  }, [SearchProd, SortProd,params.Category]);
+  }, [SearchProd, SortProd, params.Category]);
 
   useEffect(() => {
     dispatch(NextPage(0));
@@ -44,46 +44,54 @@ const Home = ({params}) => {
 
   return (
     <>
-      <RevealWrapper rotate={{ x: 22, y: 40, z: 0 }} className="load-hidden" reset={true} interval={5000} >
-        <Ban />
-      </RevealWrapper>
-      
-
-      <h2 className={`${inter.className} mt-3`} style={{ textAlign: "center", fontSize: "4000" }}>Our Products</h2>
-      <div className="container mb-3">
-        <div className="row">
-        <div style={{textAlign:"end"}}>
-      <SortControls></SortControls>
-      </div>
-        </div>
+      {Prod.length === 0 ? <div className="no-products-found">
+        <p>No products found.</p>
       </div>
 
-      <div className="container">
-        <div className="row justify-content-center">
-          {Prod.map((product) => (
-            <div key={product.id} className="col-lg-3 col-md-6 mb-4" >
-              <div className="card h-100">
-                <div className="product-image">
-                  <Image style={{ objectFit: "contain" }} src={product.imagepath} alt={product.name} width={300} height={200} className="card-img-top" />
-                </div>
-                <div className="card-body">
-                  <div className="d-flex justify-content-center flex-column align-items-center">
-                    <h5 className="card-title text-center mb-3">{product.name}</h5>
-                    <p className="card-text text-center mb-3">Company: {product.company}</p>
-                  </div>
-                  <div className="d-flex justify-content-between align-items-center">
-                    <p className="card-title">Price: ${product.price}</p>
-                    <button className="btn  add-to-cart-btn" >Add to Cart</button>
-                  </div>
-                </div>
+        :
+        <>
+          <RevealWrapper rotate={{ x: 22, y: 40, z: 0 }} className="load-hidden" reset={true} interval={5000} >
+            <Ban />
+          </RevealWrapper>
+
+
+          <h2 className={`${inter.className} mt-3`} style={{ textAlign: "center", fontSize: "4000" }}>Our Products</h2>
+          <div className="container mb-3">
+            <div className="row">
+              <div style={{ textAlign: "end" }}>
+                <SortControls></SortControls>
               </div>
             </div>
-          ))}
-        </div>
-      </div>
+          </div>
+
+          <div className="container">
+            <div className="row justify-content-center">
+              {Prod.map((product) => (
+                <div key={product.id} className="col-lg-3 col-md-6 mb-4" >
+                  <div className="card h-100">
+                    <div className="product-image">
+                      <Image style={{ objectFit: "contain" }} src={product.imagepath} alt={product.name} width={300} height={200} className="card-img-top" />
+                    </div>
+                    <div className="card-body">
+                      <div className="d-flex justify-content-center flex-column align-items-center">
+                        <h5 className="card-title text-center mb-3">{product.name}</h5>
+                        <p className="card-text text-center mb-3">Company: {product.company}</p>
+                      </div>
+                      <div className="d-flex justify-content-between align-items-center">
+                        <p className="card-title">Price: ${product.price}</p>
+                        <button className="btn  add-to-cart-btn" >Add to Cart</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
 
 
-      <Pagination></Pagination>
+          <Pagination></Pagination>
+        </>
+      }
     </>
   );
 };
