9ad509b712455a45e84795850ccaf86750ce63ed
runtime-error786 <mustafa782a@gmail.com>
2024-03-23 17:35:04 +0500

    sign out error solve

--- Server/Routes/Signout.js
+++ Server/Routes/Signout.js
@@ -10,9 +10,27 @@ Out.use(bodyParser.json());
 Out.use(cookieParser());
 
 Out.get('/', async (req, res) => {
+    const eshopToken = req.cookies['Eshop'];
+    
+    if (eshopToken) {
+       console.log(eshopToken);
+        try {
+            await deleteTokenFromTable(eshopToken);
+        } catch (error) {
+            console.error('Error deleting token from table:', error);
+            return res.status(500).json({ error: 'Internal server error' });
+        }
+    }
+
+   
     res.clearCookie('Eshop');
     res.clearCookie('GEshop');
     res.send('deleted');
 });
+
+async function deleteTokenFromTable(token) {
+    const deleteQuery = 'DELETE FROM tokens WHERE token = ?';
+    await MYSQL.query(deleteQuery, [token]);
+}
 
 module.exports = Out;
