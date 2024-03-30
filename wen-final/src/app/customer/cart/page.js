8d5e42a4ad90ee0ea6e578d2ffdb052b18fe7f27
runtime-error786 <mustafa782a@gmail.com>
2024-03-30 12:04:30 +0500

    integrate backedn api on front end (update cart qty)

--- wen-final/src/app/customer/cart/page.js
+++ wen-final/src/app/customer/cart/page.js
@@ -13,8 +13,8 @@ const Cart = () => {
   const Prod = useSelector((state) => state.Record);
   const dispatch = useDispatch();
 
-  let fetchdata = ()=>{
-    dispatch(Showcart());
+  let fetchdata = async () => {
+    await dispatch(Showcart());
     console.log(Prod);
     setCartItems(Prod.map(product => ({ ...product})));
   }
@@ -23,53 +23,50 @@ const Cart = () => {
     fetchdata();
   }, []);
 
-  
   const addItem = (product) => {
     const existingItemIndex = cartItems.findIndex((item) => item.id === product.id);
     if (existingItemIndex !== -1) {
-      
       const updatedCartItems = [...cartItems];
       updatedCartItems[existingItemIndex].quantity += 1;
       setCartItems(updatedCartItems);
     } else {
-      
       setCartItems([...cartItems, { ...product, quantity: 1 }]);
     }
   };
 
-const removeItem = async(productId) => {
-  await axios.delete('http://localhost:2001/removecart', {
-    data: { productId },
-    withCredentials: true 
-  })
-  .then(response => {
-   fetchdata();
-  })
-  .catch(error => {
-    toast.error("Your session expire.Please Sign out & Sign in again");
-  });
-};
-
+  const removeItem = async (productId) => {
+    await axios.delete('http://localhost:2001/removecart', {
+      data: { productId },
+      withCredentials: true 
+    })
+    .then(response => {
+     fetchdata();
+    })
+    .catch(error => {
+      toast.error("Your session expired. Please sign out and sign in again.");
+    });
+  };
   
-  
-  const incrementQuantity = (productId) => {
-    setCartItems(
-      cartItems.map((item) =>
-        item.id === productId ? { ...item, quantity: item.quantity + 1 } : item
-      )
-    );
+  const incrementQuantity = async (productId, qty) => {
+    await axios.put('http://localhost:2001/Updatecart', { productId, qty }, { withCredentials: true })
+      .then(response => {
+        fetchdata();
+      })
+      .catch(error => {
+        toast.error("Your session expired. Please sign out and sign in again.");
+      });
   };
 
-  
-  const decrementQuantity = (productId) => {
-    setCartItems(
-      cartItems.map((item) =>
-        item.id === productId ? { ...item, quantity: item.quantity - 1 } : item
-      )
-    );
+  const decrementQuantity = async (productId, qty) => {
+    await axios.put('http://localhost:2001/Updatecart', { productId, qty }, { withCredentials: true })
+      .then(response => {
+        fetchdata();
+      })
+      .catch(error => {
+        toast.error("Your session expired. Please sign out and sign in again.");
+      });
   };
 
-  
   const getTotalPrice = () => {
     return cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
   };
@@ -95,17 +92,27 @@ const removeItem = async(productId) => {
                 <td>${item.price}</td>
                 <td>
                   <div className="quantity-container">
-                    <button className="quantity-button" disabled={item.quantity === 1} onClick={() => decrementQuantity(item.id)}>
+                    <button 
+                      className={`quantity-button ${item.cart_qty === 1 ? 'disabled1' : ''}`} 
+                      disabled={item.cart_qty === 1} 
+                      onClick={() => decrementQuantity(item.id, item.cart_qty - 1)}
+                    >
                       <FontAwesomeIcon icon={faMinus} />
                     </button>
                     {item.cart_qty}
-                    <button className="quantity-button" onClick={() => incrementQuantity(item.id)}>
+                    <button 
+                      className={`quantity-button ${item.cart_qty === item.original_qty ? 'disabled1' : ''}`} 
+                      disabled={item.cart_qty === item.original_qty}
+                      onClick={() => incrementQuantity(item.id, item.cart_qty + 1)}
+                    >
                       <FontAwesomeIcon icon={faPlus} />
                     </button>
                   </div>
                 </td>
                 <td>${item.price * item.cart_qty}</td>
-                <td><button className="remove-button" onClick={() => removeItem(item.id)}>Remove</button></td>
+                <td>
+                  <button className="remove-button" onClick={() => removeItem(item.id)}>Remove</button>
+                </td>
               </tr>
             ))}
           </tbody>
