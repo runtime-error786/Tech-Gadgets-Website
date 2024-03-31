9b0df6021fb8406a74496c00658132a7d99d59e9
runtime-error786 <mustafa782a@gmail.com>
2024-03-31 17:13:51 +0500

    add links in footer

--- wen-final/src/app/customer/Others/Footer.js
+++ wen-final/src/app/customer/Others/Footer.js
@@ -11,23 +11,23 @@ const Footer = () => {
                     <h3>Product Categories</h3>
                     <div className="category-row">
                         <ul>
-                            <li><a className='n1' href="#">All</a></li>
-                            <li><a className='n1' href="#">Laptop</a></li>
+                            <li><a className='n1' href="/customer/all">All</a></li>
+                            <li><a className='n1' href="/customer/laptop">Laptop</a></li>
                             
                         </ul>
                         <ul>
                           
-                            <li><a className='n1' href="#">Mobile</a></li>
-                            <li><a className='n1' href="#">Desktop</a></li>
+                            <li><a className='n1' href="/customer/mobile">Mobile</a></li>
+                            <li><a className='n1' href="/customer/desktop">Desktop</a></li>
                         </ul>
                         <ul>
-                            <li><a className='n1' href="#">LED</a></li>
-                            <li><a className='n1' href="#">Watch</a></li>
+                            <li><a className='n1' href="/customer/watch">Watch</a></li>
+                            <li><a className='n1' href="/customer/tv">Tv</a></li>
                          
                         </ul>
                         <ul>
                             
-                            <li><a className='n1' href="#">Others</a></li>
+                            <li><a className='n1' href="/customer/other">Others</a></li>
                         </ul>
                     </div>
                 </div>
@@ -38,9 +38,9 @@ const Footer = () => {
                 <div className="social-media">
                     <h3>Follow Us</h3>
                     <div className="social-icons">
-                        <a href="#"><FontAwesomeIcon icon={faInstagram} /></a>
-                        <a href="#"><FontAwesomeIcon icon={faYoutube} /></a>
-                        <a href="#"><FontAwesomeIcon icon={faFacebook} /></a>
+                        <a href="https://www.instagram.com/amtatm684/"><FontAwesomeIcon icon={faInstagram} /></a>
+                        <a href="https://www.youtube.com/"><FontAwesomeIcon icon={faYoutube} /></a>
+                        <a href="https://www.facebook.com/"><FontAwesomeIcon icon={faFacebook} /></a>
                     </div>
                 </div>
             </div>
